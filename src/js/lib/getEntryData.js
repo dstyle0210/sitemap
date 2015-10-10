08dc009398501fbc48101846b7ce8c5b35f76bf9
dstyle0210 <dstyle0210@gmail.com>
2015-10-10 16:01:59 +0900

    refactory.

---
+++ src/js/lib/getEntryData.js
@@ -0,0 +1,14 @@
+_.getEntry = function(response){
+    var cells = _.map(response.feed.entry,function(data){
+        var o = {};
+        _.each(data,function(item,key){
+            if(key.indexOf("gsx$")!= -1){
+                var nkey = key.split("gsx$")[1];
+                o[nkey] = data[key].$t;
+            }
+        });
+        return o;
+    });
+    // cells = _.rest(cells); // 맨 처음 데이터를 지우고 리턴함.
+    return cells;
+};
